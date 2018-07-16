0c6afd07f870baf193a8cd012e94cbed3825fa1f
Krzysztof Stezala <icedragon@localhost.localdomain>
2018-07-16 10:08:11 +0200

    duzo

---
+++ arduino files/testServer10.ino/Headers.h
@@ -0,0 +1,5 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+#include <Ethernet.h>
+#include <SPI.h>
+#include <SD.h>
--- arduino files/testServer8.ino/testServer8.ino.ino
+++ arduino files/testServer8.ino/testServer8.ino.ino
@@ -1,14 +1,22 @@
-#include <SPI.h>
 #include <Ethernet.h>
+#include <SPI.h>
 #include <SD.h>
 
+/* JOYSTICK PART */
+int xAxis;
+int yAxis;
+
 /* STEP MOTOR PART */
-int CW_PLUS = 4;
-int CLK_PLUS = 5;
-int Distance = 0;
+// Lower step-motor
+char CW_PLUS_1 = 7; // Normal pin
+char CLK_PLUS_1 = 6; // PWM pin
+
+// Upper step-motor
+char CW_PLUS_2 = 8; // Normal pin
+char CLK_PLUS_2 = 9; // PWM pin
 
 /* FILE PART */
-//File declaration
+// File declaration
 File myFile;
 char fileName[] = "index001.htm";
 
@@ -16,57 +24,52 @@ char fileName[] = "index001.htm";
 // Enter a MAC address and IP address for your controller below.
 // The IP address will be dependent on your local network:
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
-IPAddress ip(192, 168, 1, 6);
+IPAddress ip(192, 168, 1, 100);
 
 // Initialize the Ethernet server library
 // with the IP address and port you want to use
 // (port 80 is default for HTTP):
 EthernetServer server(80);
 
-// size of buffer used to capture HTTP requests
+// Size of buffer used to capture HTTP requests
 #define REQ_BUF_SZ 90
-char HTTP_req[REQ_BUF_SZ] = {0}; // buffered HTTP request stored as null terminated string
-char req_index = 0;              // index into HTTP_req buffer
+char HTTP_req[REQ_BUF_SZ] = {0}; // Buffered HTTP request stored as null terminated string
+char req_index = 0;              // Index into HTTP_req buffer
 
 /* SETUP PART */
-//initializing serial interface
-//initializing SD card reader
-//initializing Ethernet shield
 void setup() {
-  pinMode(CW_PLUS,OUTPUT);
-  pinMode(CLK_PLUS,OUTPUT);
 
-  digitalWrite(CW_PLUS,LOW);
-  digitalWrite(CLK_PLUS,LOW);
-  // Open serial communications and wait for port to open:
   Serial.begin(9600);
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
   }
-  Serial.print("Initializing SD card...");
+  
+  //initializing pins
+  pinMode(CW_PLUS_1,OUTPUT);
+  pinMode(CLK_PLUS_1,OUTPUT);
+  
+  pinMode(CW_PLUS_2,OUTPUT);
+  pinMode(CLK_PLUS_2,OUTPUT);
+  
+  digitalWrite(CW_PLUS_1,LOW);
+  digitalWrite(CLK_PLUS_1,LOW);
+  
+  digitalWrite(CW_PLUS_2,LOW);
+  digitalWrite(CLK_PLUS_2,LOW);
+
   if (!SD.begin(4)) {
-    Serial.println("initialization failed!");
     return;
   }
-  Serial.println("initialization done.");  
   
   myFile = SD.open(fileName);
   if (myFile) {
-    Serial.println("file properly opened");
-    // read from the file until there's nothing else in it:
     myFile.close();
   } else {
-    // if the file didn't open, print an error:
-    Serial.println("error opening file");
+    return;
   }
-
-  
-  
-  // start the Ethernet connection and the server:
   Ethernet.begin(mac, ip);
+  
   server.begin();
-  Serial.print("server is at ");
-  Serial.println(Ethernet.localIP()); 
 }
 
 void loop(){
@@ -74,14 +77,11 @@ void loop(){
   EthernetClient client = server.available();
   
   if (client) {
-    
-    Serial.println("new client");
     // an http request ends with a blank line
     boolean currentLineIsBlank = true;
     while (client.connected()) {
       if (client.available()) {
         char c = client.read();
-        Serial.write(c);
         // buffer first part of HTTP request in HTTP_req array (string)
           // leave last element in array as 0 to null terminate string (REQ_BUF_SZ - 1)
          if (req_index < (REQ_BUF_SZ - 1)) {
@@ -114,33 +114,58 @@ void loop(){
               
             }
             char CHOICE = str_begin[0];
+            int Distance = 0;
+            // L - left
             if(CHOICE == 'L')
             {
-              
-              while(Distance<3600){
-                digitalWrite(CW_PLUS,LOW);
-                digitalWrite(CLK_PLUS,HIGH);
+              while(Distance<500){
+                digitalWrite(CW_PLUS_1,LOW);
+                digitalWrite(CLK_PLUS_1,HIGH);
                 delayMicroseconds(100);
-                digitalWrite(CLK_PLUS,LOW);
+                digitalWrite(CLK_PLUS_1,LOW);
                 delayMicroseconds(100);
                 Distance = Distance + 1;
               }
               Distance = 0;
-              Serial.println(CHOICE);
             }
-            else{
+            // R - right
+            else if(CHOICE == 'R'){
               
-              while(Distance<3600){
-                digitalWrite(CW_PLUS,HIGH);
-                digitalWrite(CLK_PLUS,HIGH);
+              while(Distance<500){
+                digitalWrite(CW_PLUS_1,HIGH);
+                digitalWrite(CLK_PLUS_1,HIGH);
                 delayMicroseconds(100);
-                digitalWrite(CLK_PLUS,LOW);
+                digitalWrite(CLK_PLUS_1,LOW);
                 delayMicroseconds(100);
                 Distance = Distance + 1;
               }
               Distance = 0;
+            }
+            // C - clockwise
+            else if(CHOICE == 'C'){
               
-              Serial.println(CHOICE);
+              while(Distance<500){
+                digitalWrite(CW_PLUS_1,HIGH);
+                digitalWrite(CLK_PLUS_1,HIGH);
+                delayMicroseconds(100);
+                digitalWrite(CLK_PLUS_1,LOW);
+                delayMicroseconds(100);
+                Distance = Distance + 1;
+              }
+              Distance = 0;
+            }
+            // A - counter-clockwise
+            else if(CHOICE == 'A'){
+              
+              while(Distance<500){
+                digitalWrite(CW_PLUS_1,HIGH);
+                digitalWrite(CLK_PLUS_1,HIGH);
+                delayMicroseconds(100);
+                digitalWrite(CLK_PLUS_1,LOW);
+                delayMicroseconds(100);
+                Distance = Distance + 1;
+              }
+              Distance = 0;
             }
           }
           else{
@@ -174,8 +199,15 @@ void loop(){
     // close the connection:
     char myresponse;
     myresponse = client.read();
-    client.stop();
-    Serial.println("client disconnected");
+    client.stop();    
+  }
+  else{
+    xAxis = analogRead(A1);
+    yAxis = analogRead(A0);
+    Serial.print("X:");
+    Serial.print(xAxis);
+    Serial.print(" Y:");
+    Serial.println(yAxis);
     
   }
 }
